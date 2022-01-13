fef1dbbf8f7521502f0f6ce95cb9555c2f2a1ba9
Vesqum <marcin.pelszyk30@gmail.com>
2022-01-13 19:39:02 +0100

    navigation correction

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@ const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
 
+let lastScrollY = window.scrollY;
+
+
+window.addEventListener('scroll', () => {
+    if (lastScrollY < window.scrollY) {
+        nav.classList
+    }
+})
+
 
 const handleNav = () => {
     navBtn.classList.toggle('is-active');
